1c1ed87977a81b7482b256a4d7c399be4c292d97
satyam7718 <satyamraj1903@gmail.com>
2025-12-19 11:59:58 +0530

    Intial Setup and password hashing

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const { createHmac, randomBytes } = require("crypto");
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageURl: {
+      type: String,
+      default: "/images/default.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) return;
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+});
+
+const User = model("user", userschema);
+
+module.exports = User;
